bf0bca78f1aa14766bb5887aa08728ddd2995ce4
David Weinstein <txea@Davids-MacBook-Air.local>
2017-01-20 09:31:11 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+def palindrome?(num)
+  num.to_s == num.to_s.reverse
+end
+
+def largest_palindrome_product
+  num = 0
+  (900..999).each do |i|
+    (900..999).each do |j|
+      product = i * j
+      if palindrome?(product) && product > num
+        num = product
+      end
+    end
+  end
+  num
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+
+  def initialize
+    @num = 0
+  end
+
+  def answer
+    (900..999).each do |i|
+      (900..999).each do |j|
+        product = i * j
+        if palindrome?(product) && product > @num
+          @num = product
+        end
+      end
+    end
+    @num
+  end
+end
